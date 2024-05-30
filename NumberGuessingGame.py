0faca3e9a581e7b7cc2e99224655f38ae12e7a88
Tharun <123488138+tharunsivasubramaniam@users.noreply.github.com>
2024-05-30 15:52:39 +0530

    Add files via upload
    Try again added

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -2,49 +2,58 @@ import random
 
 print("Welcome to Number Guessing Game")
 print("Are You Ready to Play With Me...Okay Let's Start The Game")
-try:
-    e = ''
-    while e != 'c':
-        print("Enter the number range that you are going to guess (EX:1,10)")
-        st, ed = map(int, input().split(","))
-        rm = random.randint(st, ed)
-        ct = ""
-        chance = 0
-        while ct != "okay":
-            gs = int(input("Guess the Number...."))
-            chance += 1
-            if st <= gs <= ed:
-                if gs == rm:
-                    print("\nWOW...!Your Guess Is Correct Brilliant")
-                    print("You Have Won Me in", end=" ")
-                    if chance == 1:
-                        print("1st Chance")
-                        print("Nega Vera Level...Ungaluku Bright Future Iruku")
-                    elif chance == 2:
-                        print("2nd Chance")
-                        print("Nega Enga Ponalu Polachupiga")
-                    elif chance == 3:
-                        print("3rd Chance")
-                        print("Paravala Unagaluku Konjo Mula Iruku")
+rst = ''
+while rst != 'r':
+    try:
+        e = ''
+        while e != 'c':
+            print("Enter the number range that you are going to guess (EX:1,10)")
+            st, ed = map(int, input().split(","))
+            rm = random.randint(st, ed)
+            ct = ""
+            chance = 0
+            while ct != "okay":
+                gs = int(input("Guess the Number...."))
+                chance += 1
+                if st <= gs <= ed:
+                    if gs == rm:
+                        print("\nWOW...!Your Guess Is Correct Brilliant")
+                        print("You Have Won Me in", end=" ")
+                        if chance == 1:
+                            print("1st Chance")
+                            print("Nega Vera Level...Ungaluku Bright Future Iruku")
+                        elif chance == 2:
+                            print("2nd Chance")
+                            print("Nega Enga Ponalu Polachupiga")
+                        elif chance == 3:
+                            print("3rd Chance")
+                            print("Paravala Unagaluku Konjo Mula Iruku")
+                        else:
+                            print(str(chance) + "th Chance")
+                        ct = "okay"
+                    elif gs > rm:
+                        print("Sorry Your Guess is too high")
+                        print("One More Chance", end=" ")
+
                     else:
-                        print(str(chance) + "th Chance")
-                    ct = "okay"
-                elif gs > rm:
-                    print("Sorry Your Guess is too high")
-                    print("One More Chance", end=" ")
+                        print("Sorry Your Guess is too low")
+                        print("One More Chance", end=" ")
 
                 else:
-                    print("Sorry Your Guess is too low")
-                    print("One More Chance", end=" ")
-
-            else:
-                print("Useless...Sorry Guess The Number Between " + str(st) + " And " + str(ed))
-                print("\n")
-        print("_____Are you like to play again press any letter rather than C____")
-        e = input("If you like to close press C : ")
-        if e == 'C':
-            e = 'c'
-
-except ValueError:
-    print("Something went wrong!")
-    print("Please check the values you entered and try again")
+                    print("Useless...Sorry Guess The Number Between " + str(st) + " And " + str(ed))
+                    print("\n")
+            print("_____Are you like to play again press any letter rather than C____")
+            e = input("If you like to close press C : ")
+            if e == 'C':
+                e = 'c'
+                rst='r'
+            if e=='c':
+                rst = 'r'
+    except ValueError:
+        print("Something went wrong!")
+        print("Please check the values you entered and Enter r to try again")
+        rst = input()
+        if rst == 'R':
+            rst='B'
+        if rst == 'r':
+            rst='B'
